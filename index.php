507d600a66f50d3f5bbb26bf054b9e1c9a79fa07
Daawson <dkujawskiii@gmail.com>
2022-04-05 07:33:17 +0200

    Initial commit
    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <link href="css/style.css" rel="stylesheet"> </style>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header">
+            <img src="img/flower_power_logo_black.png" id="logo"></img>
+            <div id="ui">
+                <a class="button">Contact</a>
+                <a class="button">Login</a>
+            </div>
+        </div>
+        <div id="main-container">
+            <div id="intro">
+                    <div class="block-50">
+                    <img class="round-img medium-img"src="img/flower_power_placeholder_bouquet.jpg"/>
+                    </div>
+                <div class="block-50">
+                <img class="center-in-block small-img" src="img/flower_power_header_bouquet.png"/>
+                    <h1>Flower Power</h1>
+                    <p>Lorem Ipsum dolor sit amet</p>
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            <p>Flower Power 2021</p>
+        </div>
+    </body>
+</html>
